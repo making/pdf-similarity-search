cc3c450ee127615f8acc71dd2915d732e729c018
Toshiaki Maki <makingx@gmail.com>
2024-01-20 00:38:36 +0900

    Use OpenAI EmbeddingClient instead of PostgresML

--- src/main/java/com/example/pdfsimilaritysearch/AppConfig.java
+++ src/main/java/com/example/pdfsimilaritysearch/AppConfig.java
@@ -1,21 +1,11 @@
 package com.example.pdfsimilaritysearch;
 
 import am.ik.accesslogger.AccessLogger;
-
-import org.springframework.ai.embedding.EmbeddingClient;
-import org.springframework.ai.embedding.PostgresMlEmbeddingClient;
-import org.springframework.ai.embedding.PostgresMlEmbeddingClient.VectorType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class AppConfig {
-
-	@Bean
-	public EmbeddingClient embeddingClient(JdbcTemplate jdbcTemplate) {
-		return new PostgresMlEmbeddingClient(jdbcTemplate, "intfloat/multilingual-e5-large", VectorType.PG_VECTOR);
-	}
 
 	@Bean
 	public AccessLogger accessLogger() {
